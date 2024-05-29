9a2d8410a4d528e2e4ecfebf0e22c939f8531b9f
bram <git@brambring.nl>
2024-05-29 13:49:07 +0200

    prepare release ExtensionUpdates 1.0.2

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This Joomla plugin checks for updates of extensions and sends an eMail once avai
 
 ### Initial setup the plugin
 
-- [Download the latest version of the plugin](https://github.com/zero-24/plg_task_extensionupdates/releases/latest)
+- [Download the latest version of the plugin](https://github.com/brbrbr/plg_task_extensionupdates/releases/latest)
 - Install the plugin using `Upload & Install`
 - Enable the plugin `Task - ExtensionUpdates` from the plugin manager
 - Setup the new Task Plugin `System -> Scheduled Tasks -> New -> ExtensionUpdates`
@@ -21,23 +21,17 @@ Any other plugin version I may have added to the download section don't get upda
 ## Issues / Pull Requests
 
 You have found an Issue, have a question or you would like to suggest changes regarding this extension?
-[Open an issue in this repo](https://github.com/zero-24/plg_task_extensionupdates/issues/new) or submit a pull request with the proposed changes.
+[Open an issue in this repo](https://github.com/brbrbr/plg_task_extensionupdates/issues/new) or submit a pull request with the proposed changes.
 
-## Translations
 
-You want to translate this extension to your own language? Check out my [Crowdin Page for my Extensions](https://joomla.crowdin.com/zero-24) for more details. Feel free to [open an issue here](https://github.com/zero-24/plg_task_extensionupdates/issues/new) on any question that comes up.
-
-## Joomla! Extensions Directory (JED)
-
-This plugin can also been found in the Joomla! Extensions Directory: [ExtensionUpdates by zero24](https://extensions.joomla.org/extension/extensionupdates/)
 
 ## Release steps
 
 - `build/build.sh`
-- `git commit -am 'prepare release ExtensionUpdates 1.0.1'`
-- `git tag -s '1.0.1' -m 'ExtensionUpdates 1.0.1'`
+- `git commit -am 'prepare release ExtensionUpdates 1.0.2'`
+- `git tag -s '1.0.1' -m 'ExtensionUpdates 1.0.2'`
 - `git push origin --tags`
-- create the release on GitHub
+- `gh release create 1.0.2 build/plg_task_extensionupdates.zip`
 - `git push origin master`
 
 ## Crowdin
--- script.php
+++ script.php
@@ -1,32 +1,104 @@
 <?php
 
-/**
- * ExtensionUpdates Task Plugin
- *
- * @copyright  Copyright (C) 2024 Tobias Zulauf All rights reserved.
- * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or later
- */
-
-defined('_JEXEC') or die;
-
-use Joomla\CMS\Installer\InstallerScript;
 
 /**
- * Installation class to perform additional changes during install/uninstall/update
- *
- * @since  1.0.0
+ * @version    1.0.2
+ * @package    BLC Packge
+ * @module     Tasks - Extensionupdates
+ * @author     Bram <bram@brokenlinkchecker.dev>
+ * @copyright  2024  Bram Brambring
+ * @license GNU General Public License version 3 or later;
  */
-class plgTaskExtensionUpdatesInstallerScript extends InstallerScript
+
+ // phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseInterface;
+use Joomla\Database\DatabaseDriver;
+use Joomla\CMS\Application\AdministratorApplication;
+use Joomla\CMS\Installer\InstallerAdapter;
+use Joomla\CMS\Installer\InstallerScriptInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\CMS\Language\Text;
+
+// phpcs:disable PSR12.Classes.AnonClassDeclaration
+return new class() implements ServiceProviderInterface
+// phpcs:enable PSR12.Classes.AnonClassDeclaration
 {
-	/**
-	 * Extension script constructor.
-	 *
-	 * @since  1.0.0
-	 */
-	public function __construct()
-	{
-		// Define the minumum versions to be supported.
-		$this->minimumJoomla = '5.1';
-		$this->minimumPhp    = '8.1';
-	}
-}
+    public function register(Container $container)
+    {
+        $container->set(
+            InstallerScriptInterface::class,
+            // phpcs:disable PSR12.Classes.AnonClassDeclaration
+            new class($container->get(AdministratorApplication::class)) implements InstallerScriptInterface
+            // phpcs:enable PSR12.Classes.AnonClassDeclaration
+            {
+                protected AdministratorApplication $app;
+                protected DatabaseDriver $db;
+
+                public function __construct(AdministratorApplication $app)
+                {
+                    $this->app = $app;
+                    $this->db  = Factory::getContainer()->get(DatabaseInterface::class);
+					$this->minimumJoomla = '5.1';
+					$this->minimumPhp    = '8.1';
+                }
+
+                public function install(InstallerAdapter $adapter): bool
+                {
+                    $query = $this->db->getquery(true);
+                    $query->update('`#__extensions`')
+                        ->set('`enabled` = 1')
+                        ->where('`type` = \'plugin\'')
+                        ->where('`folder` = ' . $this->db->quote($adapter->group))
+                        ->where('`element` = ' . $this->db->quote($adapter->element));
+                    $this->db->setQuery($query)->execute();
+                    return true;
+                }
+
+                public function update(InstallerAdapter $adapter): bool
+                {
+                    return true;
+                }
+
+                public function uninstall(InstallerAdapter $adapter): bool
+                {
+                    return true;
+                }
+				public function preflight($type, $adapter): bool
+                {
+                    if ($type !== 'uninstall') {
+                        // Check for the minimum PHP version before continuing
+                        if (version_compare(PHP_VERSION, $this->minimumPhp, '<')) {
+                            Log::add(
+                                Text::sprintf('JLIB_INSTALLER_MINIMUM_PHP', $this->minimumPhp),
+                                Log::ERROR,
+                                'jerror'
+                            );
+                            return false;
+                        }
+                        // Check for the minimum Joomla version before continuing
+                        if (version_compare(JVERSION, $this->minimumJoomla, '<')) {
+                            Log::add(
+                                Text::sprintf('JLIB_INSTALLER_MINIMUM_JOOMLA', $this->minimumJoomla),
+                                Log::ERROR,
+                                'jerror'
+                            );
+                            return false;
+                        }
+                    }
+           
+
+                    return true;
+                }
+                public function postflight(string $type, InstallerAdapter $adapter): bool
+                {
+                    return true;
+                }
+            }
+        );
+    }
+};
