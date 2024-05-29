505a5826b1ca62e3bb0f03db1938b0d96b099a87
bram <github@brambring.nl>
2024-05-29 19:49:31 +0200

    prepare release ExtensionUpdates 1.0.7

--- build/build.sh
+++ build/build.sh
@@ -4,11 +4,13 @@ EXTENSION_ELEMENT="extensionupdates"
 VERSION="1.0.7"
 if [ ! -f "$EXTENSION_ELEMENT.xml" ]; then cd ..; fi
 if [ -f "$EXTENSION_ZIP_FILENAME" ]; then rm $EXTENSION_ZIP_FILENAME; fi
+
+sed -i -e "s/\(<version>\).*\(<\/version>\)/<version>$VERSION<\/version>/g" extensionupdates.xml
+sed -i -e "s/[0-9]\+\.[0-9]\+\.[0-9]\+/$VERSION/g" README.md
+
 zip -r $EXTENSION_ZIP_FILENAME language/ "$EXTENSION_ELEMENT.xml" forms/ services/ src/ sql/ script.php --quiet
 SHA512=$(sha512sum $EXTENSION_ZIP_FILENAME | awk '{print $1}')
 sed -i -e "s/\(<sha512>\).*\(<\/sha512>\)/<sha512>$SHA512<\/sha512>/g"  \
  -e "s/\(<version>\).*\(<\/version>\)/<version>$VERSION<\/version>/g" \
  -e "s/download\/.*\/plg_task_extensionupdates.zip/download\/$VERSION\/plg_task_extensionupdates.zip/g"   update.xml
-sed -i -e "s/\(<version>\).*\(<\/version>\)/<version>$VERSION<\/version>/g" extensionupdates.xml
-sed -i -e "s/[0-9]\+\.[0-9]\+\.[0-9]\+/$VERSION/g" README.md
 echo 'package and update server ready'
